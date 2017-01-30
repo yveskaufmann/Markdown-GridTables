ab2a24e844f8e72473a814dd440d0d13f7fd4963
tcmaps <tcmaps@users.noreply.github.com>
2017-01-30 17:14:49 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from distutils.core import setup
+
+setup(
+    name='Markdown-GridTables',
+    description='ASCII tables to HTML tables.',
+    py_modules=['mdx_grid_tables'],
+    install_requires=['markdown'],
+    author='smartboyathome',
+    author_email='edit@this.com',
+    url='https://github.com/smartboyathome/Markdown-GridTables',
+    keywords='markdown, tables',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: Other/Proprietary License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+    ],
+)
